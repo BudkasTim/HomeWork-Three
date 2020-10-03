7556dfda365f58635f8417f3df8346dcd83531a6
Tim Budkas <tnamhay@gmail.com>
2020-10-03 16:26:39 -0600

    implemented upper case letter function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var upperLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerLetter="abcdefghijklmnopqrstuvwxyz";
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +13,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  var secretWord = "";
+
+  secretWord= secretWord+genUpperLetters();
+
+  return secretWord;
+}
+
+
+function genUpperLetters (){
+  // for (var i =0; i<upperLetters.length; i++ )
+  return  upperLetters[Math.floor(Math.random()*Math.floor(upperLetters.length))];
+        
+}
+
